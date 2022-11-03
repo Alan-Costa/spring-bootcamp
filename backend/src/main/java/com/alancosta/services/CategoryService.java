938646a1833e5cd21809ea9a83ceae0434724390
Alan Costa <61335987+Alan-Costa@users.noreply.github.com>
2022-11-03 09:59:40 -0300

    Add Find Category by id, exception handling

---
+++ backend/src/main/java/com/alancosta/services/CategoryService.java
@@ -0,0 +1,34 @@
+package com.alancosta.services;
+
+import com.alancosta.dto.CategoryDTO;
+import com.alancosta.entities.Category;
+import com.alancosta.repositories.CategoryRepository;
+import com.alancosta.services.exceptions.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entidade não existe"));
+        return new CategoryDTO(entity);
+    }
+
+}
